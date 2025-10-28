8e2ac6ab5b26b7133c8a533ba2e728c189790722
Paras Sharma <153740785+Paras-2005@users.noreply.github.com>
2025-10-28 13:46:35 +0530

    Create linear_search.c

---
+++ CODES/linear_search.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int arr[6] = {1,2,544,6,8,9};
+    int target = 6;
+    for(int i = 1 ;i<=6;i++){
+        if(arr[i] == target) printf("%d",i);
+    }
+  return
+}
